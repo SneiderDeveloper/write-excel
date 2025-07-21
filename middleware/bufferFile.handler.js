9e5b3bb52593d44c55f145d3b3b79bda35849f92
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2025-07-21 11:49:35 -0500

    fix: ensure proper error handling for missing file in buffer file handler middleware

--- middleware/bufferFile.handler.js
+++ middleware/bufferFile.handler.js
@@ -3,7 +3,11 @@ const path = require('path')
 const bufferFileHandler = () => {
   return (req, res, next) => {
     console.log('Buffer file handler middleware called', req.file)
-    if (!req.file) return next()
+    if (!req.file) {
+      return res.status(400).json({ 
+        error: 'No files with the extension .csv, .json, or .html were received.'  
+      })
+    }
   
     const { originalname, buffer, mimetype } = req.file
     const decodedName = decodeURIComponent(originalname)
